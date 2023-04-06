6376918d262de84fb264aba61979afc78a63db01
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-06 19:23:52 +0300

    Create Data_Types.kt

---
+++ Data_Types.kt
@@ -0,0 +1,126 @@
+In Python, there are several built-in data types available. Here are some of the most commonly used data types:
+
+
+
+1.Integer (int): 
+This data type is used to represent whole numbers, both positive and negative.
+
+2.Floating-point number (float):
+This data type is used to represent decimal numbers.
+
+3.String (str): 
+This data type is used to represent text or a sequence of characters.
+
+4.Boolean (bool):
+This data type is used to represent true or false values.
+
+5.List (list): 
+This data type is used to represent an ordered collection of items, which can be of different data types.
+
+6.Tuple (tuple):
+This data type is used to represent an ordered, immutable collection of items, which can be of different data types.
+
+7.Set (set):
+This data type is used to represent an unordered collection of unique items.
+
+8.Dictionary (dict):
+This data type is used to represent a collection of key-value pairs, where each key is unique.
+
+Python also allows you to define your own data types using classes and objects. Additionally, you can use third-party libraries to work with specialized data types like arrays,
+matrices, and graphs.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+..
