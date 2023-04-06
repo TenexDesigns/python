2b0bac60d300d4432846a121864052670a5fc95b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-06 19:21:31 +0300

    Create Variables.kt

---
+++ Variables.kt
@@ -0,0 +1,214 @@
+ Variable - Is a reuseable container for storing a value.
+ 
+ 
+ A variable behaves as if it were the value it contains
+ 
+ 
+ 
+ e.g age = 21
+ 
+ HOW TO CONNCANITATE VARIALES
+ 
+ 
+ TYPE CASTNG
+ 
+ This means to change the data type of the variable to be simmialr to where it is being used
+ 
+ e.g str(age)
+ 
+ 
+ Here is an example.
+ 
+ print("You are " + str(age) + "years old")
+ 
+ 
+ 
+ WE CAN SEPARATE THE DIFFERENT DATA TYPES
+ 
+ 
+ print ("You are ", age , ""years old)
+ 
+ 
+ WE CAN COOONACTINATE USING F-STRINGS
+ 
+ print(f"You are {age} years old")
+
+
+
+
+
+
+TRICKS
+
+
+x = 2
+y = 4
+z = 9
+
+Instead of doing the above
+ You can
+ 
+ x ,y, z = 2,4,9
+
+
+
+
+To give x ,y and z the value of 0 , we can do that like here below.
+
+
+x = y =z =0;
+
+
+
+HERE IS MORE EXPLANATION
+
+________________________________________________________________________________________________________________
+
+
+In Python, type casting refers to the conversion of one data type to another data type. 
+Python provides various built-in functions to perform type casting. Here are some examples:
+
+
+
+1.  int(): This function is used to convert a value to an integer data type.
+
+x = '10'
+print(int(x))  # Output: 10
+
+
+2.  float(): This function is used to convert a value to a floating-point data type.
+
+y = '3.14'
+print(float(y))  # Output: 3.14
+
+
+
+3.  str(): This function is used to convert a value to a string data type.
+
+z = 123
+print(str(z))  # Output: '123'
+
+
+
+4. tuple(): This function is used to convert a value to a tuple data type.
+
+
+b = [1, 2, 3]
+print(tuple(b))  # Output: (1, 2, 3)
+
+
+
+5.bool(): This function is used to convert a value to a Boolean data type.
+
+c = 0
+print(bool(c))  # Output: False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
