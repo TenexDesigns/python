475d710ddfd725dc44af564627fb0956a62fa3cf
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-06 20:33:33 +0300

    Create Indexing.kt

---
+++ Indexing.kt
@@ -0,0 +1,47 @@
+Indexing - Accesing elements of a sequence []  (indexing operator)
+                     [start :end :stop]
+
+
+credit_number = 4455-455-432;
+
+print (credit_number[0:3]) ---> Gives 445
+
+The start is included but the end is not included
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
