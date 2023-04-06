de10882248d65c97af860f4da86a2542ca782fb4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-06 19:50:11 +0300

    Create Math_In_Python.kt

---
+++ Math_In_Python.kt
@@ -0,0 +1,75 @@
+fruits = 4
+
+
+fruits = fruits +1
+
+
+or you can use AUGMENTATED ASSIGNEMENT OPERATOR  e.g +=  
+  
+  
+  fruits += 1
+
+
+
+
+
+Python has a built-in math module that provides a wide range of mathematical functions. Here are some of the most commonly used functions:
+
+
+abs(x): This function returns the absolute value of x.
+
+ceil(x): This function returns the smallest integer greater than or equal to x.
+
+floor(x): This function returns the largest integer less than or equal to x.
+
+sqrt(x): This function returns the square root of x.
+
+pow(x, y): This function returns x raised to the power of y.
+
+exp(x): This function returns the exponential value of x.
+
+log(x): This function returns the natural logarithm of x.
+
+log10(x): This function returns the base-10 logarithm of x.
+
+sin(x): This function returns the sine value of x in radians.
+
+cos(x): This function returns the cosine value of x in radians.
+
+tan(x): This function returns the tangent value of x in radians.
+
+asin(x): This function returns the inverse sine value of x in radians.
+
+acos(x): This function returns the inverse cosine value of x in radians.
+
+atan(x): This function returns the inverse tangent value of x in radians.
+
+Here is an example of how to use some of these functions:
+
+
+import math
+
+x = -4.3
+y = 2
+
+print(abs(x))          # Output: 4.3
+print(math.ceil(x))    # Output: -4
+print(math.floor(x))   # Output: -5
+print(math.sqrt(y))    # Output: 1.4142135623730951
+print(math.pow(y, 3))  # Output: 8.0
+print(math.sin(y))     # Output: 0.9092974268256817
+
+
+
+
+Regenerate response
+
+
+
+
+
+
+
+
+
+
