2f35a516787c8bd3c118f4d296b3c2728b9de7b9
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-07 11:22:09 +0300

    Update Collections_lists_sets_Tuple.kt

--- Collections_lists_sets_Tuple.kt
+++ Collections_lists_sets_Tuple.kt
@@ -6,12 +6,17 @@ List - []    - Ordered and changeable .Duplicates are OK.
 
 
 Set  - {}    - unordered and immutable/unchangeable. But add/reomve is OK, NO Duplicates allowed
+
                unordered(when displayed by print,they can appear in any order). 
                We are unable to use index on set coz they are unoerdered 
                immutable  - Meaning we cannot change a value i.e fruits[2] = "melon" is not allowed
                but ADD/REMOVE  is OK. We can add or remove items  e.g fruits.add("banana")
                No duplicates 
+
 Tuple - ()   - oredered and unchangeable . Duplictaes OK . FASTER
+
+dictionary - a collection of {key:value} pairs.
+             ordered and changeable .No duplicates
 
 
 e.g fruits = ["cherry","pear","grape","mango","avocado","orange"]
