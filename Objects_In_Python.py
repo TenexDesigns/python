049b51cd1127a631553b457d064d09b6b21212f0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-10 09:27:50 +0300

    Create Objects_In_Python.py

---
+++ Objects_In_Python.py
@@ -0,0 +1,152 @@
+In Python, everything is an object. An object is a data structure that contains data, known as attributes, and methods,
+which are functions that operate on that data. Here are some examples of objects in Python:
+
+Integer Object: An integer is an object in Python that represents a whole number.
+
+
+x = 5
+print(type(x))  # Output: <class 'int'>
+
+
+String Object: A string is an object in Python that represents a sequence of characters.
+
+s = "Hello, World!"
+print(type(s))  # Output: <class 'str'>
+
+
+List Object: A list is an object in Python that represents an ordered collection of items.
+
+lst = [1, 2, 3, 4]
+print(type(lst))  # Output: <class 'list'>
+
+
+Tuple Object: A tuple is an object in Python that represents an ordered, immutable collection of items.
+
+
+t = (1, 2, 3, 4)
+print(type(t))  # Output: <class 'tuple'>
+
+
+Dictionary Object: A dictionary is an object in Python that represents a collection of key-value pairs.
+
+
+
+d = {"name": "John", "age": 30}
+print(type(d))  # Output: <class 'dict'>
+
+
+Function Object: A function is an object in Python that represents a block of code that can be called and executed.
+
+
+  def say_hello():
+    print("Hello, World!")
+    
+print(type(say_hello))  # Output: <class 'function'>
+
+
+
+
+In summary, everything in Python is an object with its own type and attributes. This makes Python a powerful and flexible programming language.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+..
