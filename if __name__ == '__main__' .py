121a51410b82a3b49d35cff8e66a4f752c00199f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-11 07:09:38 +0300

    Update and rename Python if __name__ == '__main__' .py to if __name__ == '__main__' .py

--- if __name__ == '__main__' .py
+++ if __name__ == '__main__' .py
@@ -1,3 +1,16 @@
+ y tho?
+ 1. Module can be run as a standalone program
+    or
+ 2. Module can be imported and used by other modules
+
+  Python interpreter sets "special variables", one of which is _name_
+  Python will assign the _name_ variable a value of '__main__' if it's
+  the initial module being run
+
+
+
+
+
 if __name__ == '__main__' is a common line of code you may see at the bottom of a Python script.
 It is used to separate the code that should be executed when the script is run directly from the code that should be executed when the script is 
 imported as a module into another script.
